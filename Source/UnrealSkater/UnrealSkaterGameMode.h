c6b8c764c7d277501a2b392ee533662e3f816a69
FIlipe Muniz <munizbusiness312@gmail.com>
2024-06-09 09:32:56 -0300

    Update: Creating Unreal's ThirdPersonCharacterTemplate

---
+++ Source/UnrealSkater/UnrealSkaterGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealSkaterGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUnrealSkaterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUnrealSkaterGameMode();
+};
+
+
+
